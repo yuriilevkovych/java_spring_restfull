ee3befdfdb93cb437b632c6311383a6b7e925b56
yuriilevkovich <yurii.levkovich@gmail.com>
2022-09-12 17:38:04 +0200

    Restfull person sample with SOLID principe

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -21,5 +22,35 @@ public class FakePersonDataAccessService implements com.example.demo.dao.PersonD
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> maybePerson = selectPersonById(id);
+        if (maybePerson.isEmpty()) {
+            return 0;
+        }
+
+        DB.remove(maybePerson.get());
+
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+            .map(person -> {
+                int indexOfPersonToUpdate = DB.indexOf(person);
+                if (indexOfPersonToUpdate >= 0) {
+                    DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                }
+                return 1;
+            })
+            .orElse( 0);
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
     }
 }
--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -14,4 +15,10 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+    Optional<Person> selectPersonById(UUID id);
 }
